67a4d5673eaff9257bcda4d3a0fff4ace67c082e
icaropy <icarobatista02@hotmail.com>
2016-07-24 18:29:08 -0300

    Add files via upload

---
+++ buscaestado.py
@@ -0,0 +1,55 @@
+"""Programa que perdi DD e monstre as cidade"""
+print("Consulta de estado ")
+
+
+sp = 11 
+ac = 68
+am = 97
+bahia = 77
+rj = 24 
+go = 62 
+al = 82 
+ce_s = 88
+es = 27 
+mg = 31 
+df = 61
+ma = 99 
+pr =  42 
+
+dd = int(input("Digitem o DD: "))
+
+if dd == sp:
+    print("Este DD pertencem ao estado de  'são paulo'")
+else :
+    print("Nada consta")
+if dd == ac:
+    print("Este DD pertencem ao estado do 'Acre'")
+if dd == am :
+    print("Este DD pertencem ao estado de 'Amazona'")
+if dd == bahia :
+    print("Este DD pertencem ao estado de 'Bahia'")
+if dd == rj :
+    print("Este DD pertencem ao estado de 'Rio de Janeiro'")
+if dd == go :
+    print("Este DD pertencem ao estado de 'Goias'")
+if dd == al :
+    print("Este DD pertencem ao estado de 'Alagoas'")
+if dd == ce_s :
+    print("Este DD pertencem ao estado de 'Ceara'")
+if dd == es :
+    print("Este DD pertencem ao estado de 'Espirito Santo")
+if dd == mg :
+    print("Este DD pertencem ao estado de 'Minas Gerais'")
+if dd == df :
+    print("Este DD pertencem ao estado do ' Distrito Federal '")
+if dd == ma :
+    print("Este DD pertencem ao estado de 'Maranhão'")
+if dd == pr :
+    print("Este DD pertencem ao estado de 'Paraná'")
+    
+
+
+
+
+
+ 
